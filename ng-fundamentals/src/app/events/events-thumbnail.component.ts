4990b6c25a7363ca67382e4f281ee2b07dcbf41b
Dlloydkirch <dlloydkirch@gmail.com>
2020-06-25 16:25:56 -0500

    ng switch

--- ng-fundamentals/src/app/events/events-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/events-thumbnail.component.ts
@@ -7,8 +7,13 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
         <h2> {{event?.name}} </h2>
         <div>Date: {{event?.date}}</div>
         <div>Time: {{event?.time}}</div>
+        <div [ngSwitch]="event?.time">
+            <span *ngSwitchCase="'8:00 am'">Early Start</span>
+            <span *ngSwitchCase="'10:00 am'">Late Start</span>
+            <span *ngSwitchDefault>Normal Start</span>
+        </div>
         <div>Price: \${{event?.price}}</div>
-        <div [hidden]="!event?.location">
+        <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
