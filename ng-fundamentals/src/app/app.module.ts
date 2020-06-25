97b3c1248a364542f242523b9a2dfcbb39e557d1
Dlloydkirch <dlloydkirch@gmail.com>
2020-06-25 15:56:34 -0500

    adding a menu/navigation bar

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
-
+import { NavbarComponent } from './nav/navbar.component';
 
 //source location from github - https://github.com/jmcooper/angular-fundamentals-files
 
@@ -15,7 +15,8 @@ import { EventThumbnailComponent } from './events/events-thumbnail.component';
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   bootstrap: [EventsAppComponent]
 })
