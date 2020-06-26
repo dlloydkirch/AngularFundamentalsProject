0dd5448156bb0a4931a20de0a27642134a1d562b
Dlloydkirch <dlloydkirch@gmail.com>
2020-06-26 10:07:00 -0500

    adding link to events

--- ng-fundamentals/src/app/events/events-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/events-thumbnail.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class="well hoverwell thumbnail">
+    <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
         <h2> {{event?.name}} </h2>
         <div>Date: {{event?.date}}</div>
         <div [ngStyle]=getStartTimeStyle() [ngSwitch]="event?.time" >
@@ -51,5 +51,5 @@ export class EventThumbnailComponent{
             return {color: '#003300', 'font-weight': 'bold'}
         return {}
     }
-    
+
 }
