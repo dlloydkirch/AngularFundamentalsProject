0b3f7dbd472e4c92c194e410f33a09518066c07b
Dlloydkirch <dlloydkirch@gmail.com>
2020-06-26 11:23:22 -0500

    adding barrels

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'
+
+import{
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponet,
+  EventsListResolver,
+  EventRouteActivator
+}from './events/index'
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/events-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { appRoutes } from './nav/routes'
-import { Router, RouterModule } from '@angular/router'
-import { CreateEventComponet } from './events/create-event.component';
+import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventsListResolver } from './events/events-list-resolver.service';
 //source location from github - https://github.com/jmcooper/angular-fundamentals-files
 
 @NgModule({
---
+++ ng-fundamentals/src/app/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router'
+import { Error404Component } from './errors/404.component'
+
+import {
+    EventsListResolver,
+    EventRouteActivator,
+    CreateEventComponet,
+    EventDetailsComponent,
+    EventsListComponent
+} from './events/index'
+
+export const appRoutes:Routes = [ 
+    { path : 'events/new', component: CreateEventComponet, canDeactivate: ['canDeactivateCreateEvent']},
+    { path : 'events', component: EventsListComponent, resolve: {events:EventsListResolver}},
+    
+    { path : 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
+    { path : '404', component: Error404Component},
+    { path : '', redirectTo: '/events', pathMatch: 'full'},
+    { path : 'user', loadChildren: './user/user.module#UserModule'}
+]
